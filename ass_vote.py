206f573d153b5c32f65f726f1d2a4ce6569167be
frankhjh <73477765+frankhjh@users.noreply.github.com>
2021-06-05 21:23:23 +0800

    Update ass_vote.py

--- ass_vote.py
+++ ass_vote.py
@@ -11,11 +11,9 @@ def Ass_vote(addr): #addr means the address of smart contract
     #here I just set number of voters and amount assigned to each voter both equal 5
     num_voters=5
     amount_per_voter=5
-    #here i use the first address >>w3.eth.accounts[0]<< as the contract constructor 
-    
     print('Start assigning votes...')
     for i in range(1,num_voters+1):
-        vote_contract.functions.Give_right_to_voters(w3.eth.accounts[i],amount_per_voter).transact(transaction={'from':w3.eth.accounts[0]})
+        vote_contract.functions.Give_right_to_voters(w3.eth.accounts[i],amount_per_voter).transact(transaction={'from':w3.eth.accounts[0]})#here i use the first address-->w3.eth.accounts[0] as the contract constructor 
     print('Assigned Done!')
     return w3,vote_contract
 
